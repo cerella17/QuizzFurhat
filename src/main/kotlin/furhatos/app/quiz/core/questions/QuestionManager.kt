949b6fa47b58fd5c2c33bfff8313bac816569477
cerella17 <giuseppecerella18@gmail.com>
2024-09-16 20:49:02 +0200

    question

--- src/main/kotlin/furhatos/app/quiz/core/questions/QuestionManager.kt
+++ src/main/kotlin/furhatos/app/quiz/core/questions/QuestionManager.kt
@@ -28,214 +28,171 @@ class QuestionSet {
     private fun loadQuestions() {
         val jsonText = """
             [
-              {
-                "question": "Quale attributo HTML imposta un testo alternativo per un’immagine?",
-                "answers": [
-                  "alt",
-                  "la prima",
-                  "la A",
-                  "la risposta uno",
-                  "prima",
-                  "A",
-                  "uno"
-                ],
-                "options": [
-                  "alt",
-                  "src",
-                  "style",
-                  "size"
-                ]
-              },
-              {
-                "question": "Quale proprietà CSS controlla la dimensione del testo?",
-                "answers": [
-                  "font size",
-                  "la terza",
-                  "la tre",
-                  "la C",
-                  "C",
-                  "tre",
-                  "terza",
-                  "la numero tre"
-                ],
-                "options": [
-                  "Text-size",
-                  "Text-style",
-                  "Font-size",
-                  "Font-style"
-                ]
-              },
-              {
-                "question": "In JavaScript, come si invoca una funzione denominata “ciao()”?",
-                "answers": [
-                  "ciao parentesi tonde",
-                  "ciao",
-                  "la seconda",
-                  "la due",
-                  "la b",
-                  "b",
-                  "due",
-                  "seconda",
-                  "la numero 2"
-                ],
-                "options": [
-                  "Tutte le opzioni",
-                  "ciao()",
-                  "call function ciao()",
-                  "call ciao()"
-                ]
-              },
-              {
-                "question": "Dov’è il posto giusto dove inserire un codice JavaScript?",
-                "answers": [
-                  "Sia la sezione head che body sono corrette",
-                  "sia head che body",
-                  "head e body",
-                  "sia head e sia body",
-                  "sia head sia body",
-                  "Sia la sezione ed che body sono corrette",
-                  "sia ed che body",
-                  "ed e body",
-                  "sia ed e sia body",
-                  "sia ed sia body",
-                  "la prima",
-                  "la A",
-                  "la risposta uno",
-                  "prima",
-                  "A",
-                  "uno"
-                ],
-                "options": [
-                  "Sia la sezione <head> che <body> sono corrette",
-                  "Il codice JavaScript non può essere inserito",
-                  "Nella sezione <body>",
-                  "Nella sezione <head>"
-                ]
-              },
-              {
-                "question": "In JavaScript, quale evento si verifica quando l’utente clicca su un element HTML?",
-                "answers": [
-                  "onclick",
-                  "un click",
-                  "on click",
-                  "la prima",
-                  "la A",
-                  "la numero uno",
-                  "A",
-                  "la risposta A",
-                  "prima",
-                  "uno"
-                ],
-                "options": [
-                  "Onclick",
-                  "Onchange",
-                  "Onmouseclick",
-                  "Onmouseover"
-                ]
-              },
-              {
-                "question": "Come dichiari una variabile intera in JavaScript?",
-                "answers": [
-                  "var variabile",
-                  "bar variabile",
-                  "la prima",
-                  "la numero uno",
-                  "la risposta A",
-                  "la a",
-                  "A",
-                  "prima",
-                  "uno"
-                ],
-                "options": [
-                  "Var variabile",
-                  "Var variabile int",
-                  "Variable variabile",
-                  "Int variabile"
-                ]
-              },
-              {
-                "question": "Come si seleziona l’elemento con id “demo”?",
-                "answers": [
-                  "#demo",
-                  "hashtag demo",
-                  "astag demo",
-                  "ashtag demo",
-                  "cancelletto demo",
-                  "la seconda",
-                  "la due",
-                  "la b",
-                  "la risposta due",
-                  "due",
-                  "b",
-                  "la seconda risposta",
-                  "la risposta b"
-                ],
-                "options": [
-                  ".demo",
-                  "#demo",
-                  "Demo",
-                  "*demo"
-                ]
-              },
-              {
-                "question": "In CSS, quale è lo statement che consente di non visualizzare un particolare elemento (tag)?",
-                "answers": [
-                  "Display none",
-                  "Display non",
-                  "la terza",
-                  "la risposta tre",
-                  "la risposta c",
-                  "la c",
-                  "la tre",
-                  "c",
-                  "tre"
-                ],
-                "options": [
-                  "Display: initial",
-                  "Display: hidden",
-                  "Display: none",
-                  "Nessuna delle opzioni"
-                ]
-              },
-              {
-                "question": "Il seguente selettore JQuery $(“div”) cosa seleziona?",
-                "answers": [
-                  "Tutti gli elementi div",
-                  "Tutti gli elementi di",
-                  "la terza",
-                  "la risposta tre",
-                  "la risposta c",
-                  "la c",
-                  "la tre",
-                  "c",
-                  "tre"
-                ],
-                "options": [
-                  "L’HTML del primo elemento div",
-                  "Nessuna delle opzioni",
-                  "Tutti gli elementi div",
-                  "Il primo element div"
-                ]
-              },
-              {
-                "question": "Quale simbolo si utilizza come scorciatoia per jQuery?",
-                "answers": [
-                  "Il simbolo dollaro",
-                  "il dollaro",
-                  "dollaro",
-                  "la terza",
-                  "la tre",
-                  "la C",
-                  "C",
-                  "tre"
-                ],
-                "options": [
-                  "Il simbolo %",
-                  "Il simbolo ?",
-                  "Il simbolo $",
-                  "Il simbolo #"
-                ]
-              }
+               {
+                  "question":"In CSS, quale delle seguenti proprietà di un tag ancora implica un collegamento ipertestuale ancora non visitato? ",
+                  "answers":[
+                     "link",
+                     "la prima",
+                     "la A",
+                     "la risposta uno",
+                     "prima",
+                     "A",
+                     "uno"
+                  ],
+                  "options":[
+                     "Link",
+                     "Visited",
+                     "Hover",
+                     "Active"
+                  ]
+               },
+               {
+                  "question":"Quale proprietà CSS controlla la dimensione del testo?",
+                  "answers":[
+                     "font size",
+                     "la terza",
+                     "la tre",
+                     "la C",
+                     "C",
+                     "tre",
+                     "terza",
+                     "la numero tre"
+                  ],
+                  "options":[
+                     "Text-size",
+                     "Text-style",
+                     "Font-size",
+                     "Font-style"
+                  ]
+               },
+               {
+                  "question":"In JavaScript, come si invoca una funzione denominata “ciao()”?",
+                  "answers":[
+                     "ciao parentesi tonde",
+                     "ciao",
+                     "la seconda",
+                     "la due",
+                     "la b",
+                     "b",
+                     "due",
+                     "seconda",
+                     "la numero 2"
+                  ],
+                  "options":[
+                     "Tutte le opzioni",
+                     "ciao()",
+                     "call function ciao()",
+                     "call ciao()"
+                  ]
+               },
+               {
+                  "question":"In JavaScript, quale evento si verifica quando l’utente clicca su un element HTML?",
+                  "answers":[
+                     "onclick",
+                     "un click",
+                     "on click",
+                     "la prima",
+                     "la A",
+                     "la numero uno",
+                     "A",
+                     "la risposta A",
+                     "prima",
+                     "uno"
+                  ],
+                  "options":[
+                     "Onclick",
+                     "Onchange",
+                     "Onmouseclick",
+                     "Onmouseover"
+                  ]
+               },
+               {
+                  "question":"Quale delle seguenti tecnologie consente di ottenere i dati da un server senza ricaricare l'intera pagine?",
+                  "answers":[
+                     "AJAX",
+                     "agiax",
+                     "aiax",
+                     "la prima",
+                     "la numero uno",
+                     "la risposta A",
+                     "la a",
+                     "A",
+                     "prima",
+                     "uno"
+                  ],
+                  "options":[
+                     "AJAX",
+                     "JSON",
+                     "Javascript",
+                     "MVC"
+                  ]
+               },
+               {
+                  "question":"Come si seleziona l’elemento con id “demo”?",
+                  "answers":[
+                     "#demo",
+                     "hashtag demo",
+                     "astag demo",
+                     "ashtag demo",
+                     "cancelletto demo",
+                     "la seconda",
+                     "la due",
+                     "la b",
+                     "la risposta due",
+                     "due",
+                     "b",
+                     "la seconda risposta",
+                     "la risposta b"
+                  ],
+                  "options":[
+                     ".demo",
+                     "#demo",
+                     "Demo",
+                     "*demo"
+                  ]
+               },
+               {
+                  "question":"In CSS, quale è lo statement che consente di non visualizzare un particolare elemento (tag)?",
+                  "answers":[
+                     "Display none",
+                     "Display non",
+                     "display non è",
+                     "la terza",
+                     "la risposta tre",
+                     "la risposta c",
+                     "la c",
+                     "la tre",
+                     "c",
+                     "tre"
+                  ],
+                  "options":[
+                     "Display: initial",
+                     "Display: hidden",
+                     "Display: none",
+                     "Nessuna delle opzioni"
+                  ]
+               },
+               {
+                  "question":"La sessione tra più richieste di uno stesso client può essere mantenuta:",
+                  "answers":[
+                     "utilizzando HttpSession",
+                     "HttpSession",
+                     "http session",
+                     "la terza",
+                     "la risposta tre",
+                     "la risposta c",
+                     "la c",
+                     "la tre",
+                     "c",
+                     "tre"
+                  ],
+                  "options":[
+                     "Utilizzando PageContext",
+                     "Tutte le opzioni",
+                     "Utilizzando HttpSession",
+                     "Utilizzando SessionManager"
+                  ]
+               }
             ]
         """.trimIndent()
         val questionsList = Json.decodeFromString<List<Question>>(jsonText)
